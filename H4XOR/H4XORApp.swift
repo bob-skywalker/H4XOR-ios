3ca081039d11f59452004ecc56b52dc989e66983
bob-skywalker <q463250938@gmail.com>
2022-12-23 17:57:02 -0800

    Initial Commit

---
+++ H4XOR/H4XORApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by bo zhong on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
